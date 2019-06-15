683f3556100a997f26da1e818d9cf86e23719e9a
ExpiredPopsicle <expiredpopsicle@gmail.com>
2019-06-15 16:21:57 -0700

    Simplified and documented some stuff. Fixed paths.

--- examples/autotools-project/src/main.cpp
+++ examples/autotools-project/src/main.cpp
@@ -2,25 +2,10 @@
 
 #include <iostream>
 
-#ifndef PACKAGE_TARNAME
-#define PACKAGE_TARNAME backup_butts
-#endif
-
 int main(int argc, char *argv[])
 {
-    std::cout << "Holy shit it worked." << std::endl;
-
-    std::cout << "My path is: " << ExPop::FileSystem::getExecutablePath(argv[0]) << std::endl;
-
-    std::cout << "My data is in: "
-              << ExPop::FileSystem::makeFullPath(
-                  ExPop::FileSystem::getExecutablePath(argv[0]) + "/../share")
+    std::cout << "My path is: "
+              << ExPop::FileSystem::getExecutablePath(argv[0])
               << std::endl;
-
-    std::cout << "My data might also be in: "
-              << ExPop::FileSystem::makeFullPath(
-                  ExPop::FileSystem::getExecutablePath(argv[0]) + "/../share/" PACKAGE_TARNAME)
-              << std::endl;
-
     return 0;
 }
